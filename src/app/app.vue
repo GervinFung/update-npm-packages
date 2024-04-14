d851a91ed18188790626c9e920f8829dcf932ff9
GervinFung <gervinfungdaxuen@gmail.com>
2024-04-14 20:35:20 +0800

    setup(tailwind): added necessary dependencies for tailwind

--- src/app/app.vue
+++ src/app/app.vue
@@ -2,4 +2,7 @@
 import { getRepos } from '../logic/get-repos';
 </script>
 
-<template>{{ JSON.stringify(getRepos()) }}</template>
+<template>
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+	{{ JSON.stringify(getRepos()) }}
+</template>
